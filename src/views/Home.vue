c342ea07bb0cbd9ded255575926f8d40da119fc4
LeeHanna <71548623+lee1nna@users.noreply.github.com>
2021-10-15 02:43:18 +0900

    v-for를 사용해 배열 데이터 처리하기

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,6 +4,17 @@
         <input type="text" v-model="input1"/>
         <button type="button" @click="getData">Get</button>
         <button type="button" @click="setData">Set</button>
+
+        <select class="form-control" v-model="region" @change="changeRegion">
+            <option :key="i" :value="d.v" v-for="(d,i) in options">{{d.t}}</option>
+        </select>
+
+        <table class="table table-bordered" v-show="tableShow">
+            <tr :key="i" v-for="(d,i) in options">
+                <td>{{d.v}}</td>
+                <td>{{d.t}}</td>
+            </tr>
+        </table>
     </div>
 </template>
 
@@ -14,7 +25,19 @@ export default {
             title: "개발자의 품격",
             title2: "Seoul",
             input1: "abcde",
+            options: [
+                {v:"S", t:"Seoul"},
+                {v:"J", t:"Jeju"},
+                {v:"B", t:"Busan"},
+            ],
+            region:"J",
+            tableShow:true,
         };
+    },
+    watch: {
+        input1(){
+            console.log(this.input1);
+        }
     },
     methods: {
         getData(){
@@ -22,6 +45,9 @@ export default {
         },
         setData(){
             this.input1="1234";
+        },
+        changeRegion(){
+            alert(this.region);
         }
     },
     beforeCreate() {
