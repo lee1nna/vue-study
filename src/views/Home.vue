cb392e4f71a0109f7cdd6b4d18b4ba3b537262a7
LeeHanna <71548623+lee1nna@users.noreply.github.com>
2021-10-15 02:25:15 +0900

    vue의 life-cycle

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -24,6 +24,29 @@ export default {
             this.input1="1234";
         }
     },
-
+    beforeCreate() {
+        console.log("beforeCreate")
+    },
+    created() {
+        console.log("created")
+    },
+    beforeMount() {
+        console.log("beforeMount")
+    },
+    mounted() {
+        console.log("mounted")
+    },
+    beforeUpdate(){
+        console.log("beforeUpdate")
+    },
+    updated(){
+    console.log("updated") 
+    },
+    beforeDestroy(){
+        console.log("beforeDestroy")
+    },
+    destroyed(){
+        console.log("destroyed")
+    }
 }
 </script>
